52ea5c458d6b6942efc6023da9891e3f2c31d31f
divyanshchahar <divyanshchahar@outlook.com>
2023-01-11 13:58:59 +0530

    added heading

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="heading-1">
+        <h1>Habit Tracker</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
